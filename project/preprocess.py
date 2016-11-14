2403c2859503759817ce1ba14d99391ae9e58b02
anbarasimanoharan <anbarasi.avila@gmail.com>
2016-11-14 02:16:30 -0500

    preprocess-basics

--- project/preprocess.py
+++ project/preprocess.py
@@ -1,19 +1,51 @@
 from sklearn import preprocessing
-import tableReader
+import tableReader, math, numpy
 
 class preprocess:
+    def norm(self, table):
+        for colPos in range(len(table.cols)-1):
+	        allValues = [] 
+	        for row in table.rows:
+	            allValues.append(row[colPos])
+	        maxValue = max(allValues)
+	        minValue = min(allValues)
+	        print minValue, maxValue
 
-	allValues = [] 
+	        for i, actual_value in enumerate(allValues):
+	            table.rows[i][colPos] =  float(actual_value - minValue)/(maxValue - minValue) 
 
-	def norm(table):
-		normValue = float(actual_value - min(allValues))/(max(max(allValues) - min(allValues)),1) 
-		
-	def width5bin(table):
-		minVal = min(allValues)
+        return table
 
-		interval = float(max(allValues) - minVal) / 5
-		bins = []
-		for i in range(5):
-			bins.append(minVal + interval*i )
+    def logarithm(self, table):
+    	for i in range(len(table.cols)-1):
+            for row in table.rows:
+                row[i] = math.log(row[i])
+    	
+        return table
 
-		print numpy.digitize(allValues, bins)
+    def width5bin(self, table):        
+        for colPos in range(len(table.cols)-1):
+            allValues = [] 
+            for row in table.rows:
+                allValues.append(row[colPos])
+            maxValue = max(allValues)
+            minValue = min(allValues)
+
+            binWidth = (maxValue - minValue)/5
+
+            bins = []
+            for i in range(5):
+                bins.append(minValue + binWidth*i )
+
+            allValues = numpy.digitize(allValues, bins)
+
+            for i, actual_value in enumerate(allValues):
+                table.rows[i][colPos] =  actual_value 
+        return table
+
+	def freq5bin(table):
+		table.sort()
+		result = []
+		length = len(table)
+		# for i in range(5):
+		# 	temp = 
